4df467594cd8fe7ec0f6fade1dc035e566c96195
Akintunde Ilerioluwa <ilerioluwac@gmail.com>
2024-10-07 15:12:00 +0100

    linked courses to homepage

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -96,7 +96,8 @@ const Navbar = () => {
                       {courses.map((course) => (
                         <Link
                           key={course.id}
-                          href={course.href}
+                          // href={course.href}
+                          href="/"
                           className="block px-4 py2 py-4 text-[0.9rem] text-white hover:bg-slate-900"
                           role="menuitem"
                           onClick={() => setShowDropdown(false)}
@@ -173,7 +174,8 @@ const Navbar = () => {
                   {courses.map((course) => (
                     <Link
                       key={course.id}
-                      href={course.href}
+                      // href={course.href}
+                      href="/"
                       className="block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:bg-slate-950"
                       onClick={() => {
                         setIsOpen(false);
