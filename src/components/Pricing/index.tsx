434608bb1a656a77f56a7f3e8ca58672406505a4
Marvelousy20 <afosmarvel@gmail.com>
2024-11-05 18:20:42 +0100

    removed number of recurrent payments

--- src/components/Pricing/index.tsx
+++ src/components/Pricing/index.tsx
@@ -47,9 +47,9 @@ const PricingCard: React.FC<PricingCardProps> = ({
           </p> */}
           <span className="text-[3.5rem] lg:text-[3.75rem] ml-4 font-medium">
             {currentPrice}
-            <span className="text-2xl font-normal">
+            {/* <span className="text-2xl font-normal">
               {noOfMonths ? noOfMonths : ""}
-            </span>
+            </span> */}
           </span>
         </div>
       </div>
