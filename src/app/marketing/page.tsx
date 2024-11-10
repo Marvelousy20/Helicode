96fafc8b23f9cc0966d8314c10da45887b25ca44
Marvelousy20 <afosmarvel@gmail.com>
2024-11-10 13:32:00 +0100

    wip

--- src/app/marketing/page.tsx
+++ src/app/marketing/page.tsx
@@ -149,7 +149,7 @@ export default function Page() {
       imgLink: "/Hashtag-Square.svg",
       heading: "Start Date",
       // text: `${data?.data?.map((item) => item?.startDate) || "N/A"}`,
-      text: "2024-11-11",
+      text: "November, 2024",
       cohortTwo: "January, 2025",
     },
     {
