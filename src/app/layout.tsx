c78a3b36c67228cc1a06d3b5ffce76e42ca2c648
Marvelousy20 <afosmarvel@gmail.com>
2025-02-06 11:18:59 +0100

    added meta tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,8 @@ import Providers from "@/lib/providers";
 import { Toaster } from "react-hot-toast";
 import "react-toastify/dist/ReactToastify.css";
 const inter = Inter({ subsets: ["latin"] });
-import EmailCaptureModal from "@/components/EmailCaptureModal";
+// import EmailCaptureModal from "@/components/EmailCaptureModal";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "School made for the future",
@@ -22,9 +23,27 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script id="fb-pixel" strategy="afterInteractive">
+          {`!function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '906902701550243');
+            fbq('track', 'PageView');`}
+        </Script>
+        <noscript>
+          {`<img height="1" width="1" style="display:none"
+          src="https://www.facebook.com/tr?id=906902701550243&ev=PageView&noscript=1"
+          />`}
+        </noscript>
+      </head>
       <body className={inter.className}>
         <Providers>
-          {/* <main className="px-5 lg:px-20 bg-[#010115] text-white"> */}
           <main className="bg-[#010115] text-white">
             <Navbar />
             {/* <EmailCaptureModal /> */}
@@ -36,4 +55,11 @@ export default function RootLayout({
       </body>
     </html>
   );
+}
+
+{
+  /* <script>
+
+</script>
+<noscript></noscript> */
 }
