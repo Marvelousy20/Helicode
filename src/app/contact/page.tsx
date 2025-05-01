d5b8065985aa864dacd05afe7dcc1fad6f818b40
Marvelousy20 <afosmarvel@gmail.com>
2025-05-01 03:11:35 +0100

    wip

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -64,7 +64,7 @@ export default function ContactUs() {
 
             <div className="flex items-center gap-2.5">
               <a
-                href="https://www.instagram.com/helicodeacademy?igsh=YWxxbWc3NGxmdDhv"
+                href="https://www.instagram.com/helicodehq"
                 className="flex items-center gap-2.5"
               >
                 {/* <span className="h-9 w-9 bg-[#8D58FF1A] rounded-[8px] flex items-center justify-center"> */}
